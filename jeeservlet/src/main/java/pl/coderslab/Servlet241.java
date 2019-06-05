c785cd97b369a9280c7c2a31fba0a331fbaf157b
michalsurowiec <michalsurowiec94@o2.pl>
2019-06-05 10:25:43 +0200

    Zadanie domowe 3 tydzien 2 dzien - czwarte zadanie czesciowo zrobione

--- jeeservlet/src/main/java/pl/coderslab/Servlet241.java
+++ jeeservlet/src/main/java/pl/coderslab/Servlet241.java
@@ -1,8 +1,21 @@
 package pl.coderslab;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/servlet241")
 public class Servlet241 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        for (int i = 0; i <=7; i++) {
+            resp.getWriter().println("<a href='http://localhost:8080/jeeservlet_war_exploded/servlet242?id=" + i + "'>Id " + i + "</a> <br>");
+        }
+
+    }
 }
